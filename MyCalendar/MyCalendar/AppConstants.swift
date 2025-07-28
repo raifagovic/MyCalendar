879c743a2ceccec3b441e82571a18d215cc80449
Raif Agovic <raif.agovic.dev@gmail.com>
2025-07-28 16:17:53 +0200

    Create AppConstants struct

---
+++ MyCalendar/MyCalendar/AppConstants.swift
@@ -0,0 +1,16 @@
+//
+//  AppConstants.swift
+//  MyCalendar
+//
+//  Created by Raif Agovic on 28. 7. 2025..
+//
+
+import Foundation
+import CoreGraphics
+
+// A place for globally used constants
+struct AppConstants {
+    // A cell that is taller than it is wide.
+    // Example: width = 75, height = 100. Ratio = 75/100 = 0.75
+    static let calendarCellAspectRatio: CGFloat = 3.0 / 4.0
+}
