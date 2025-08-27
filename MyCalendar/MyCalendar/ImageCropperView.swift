92514fd32c7b10e3dd0cea0cbec935887bdf037c
Raif Agovic <raif.agovic.dev@gmail.com>
2025-08-27 20:47:09 +0200

    fix(DayDetailView): Resolve incorrect initial scale for new background images

--- MyCalendar/MyCalendar/ImageCropperView.swift
+++ MyCalendar/MyCalendar/ImageCropperView.swift
@@ -19,40 +19,6 @@ struct ImageCropperView: View {
     @GestureState private var gestureScale: CGFloat = 1.0
     @GestureState private var gestureOffset: CGSize = .zero
 
-//    var body: some View {
-//        let magnificationGesture = MagnificationGesture()
-//            .updating($gestureScale) { value, state, _ in
-//                state = value
-//            }
-//            .onEnded { value in
-//                // Update the final scale by multiplying with the gesture's change
-//                scale *= value
-//            }
-//
-//        let dragGesture = DragGesture()
-//            .updating($gestureOffset) { value, state, _ in
-//                state = value.translation
-//            }
-//            .onEnded { value in
-//                // Update the final offset by adding the gesture's translation
-//                offsetX += value.translation.width
-//                offsetY += value.translation.height
-//            }
-//        
-//        // Combine both gestures so they can be used simultaneously
-//        let combinedGesture = SimultaneousGesture(magnificationGesture, dragGesture)
-//
-//        if let uiImage = UIImage(data: imageData) {
-//            Image(uiImage: uiImage)
-//                .resizable()
-//                .scaledToFill()
-//                // Apply the combined scale (final scale * live gesture scale)
-//                .scaleEffect(scale * gestureScale)
-//                // Apply the combined offset
-//                .offset(x: offsetX + gestureOffset.width, y: offsetY + gestureOffset.height)
-//                .gesture(combinedGesture) // Attach the combined gesture to the image
-//        }
-//    }
     var body: some View {
         // These gesture definitions are still necessary.
         let magnificationGesture = MagnificationGesture()
