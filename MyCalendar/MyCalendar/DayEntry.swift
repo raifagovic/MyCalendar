41fe7d713181693ebda89f3e61385a1fe31521da
Raif Agovic <raif.agovic.dev@gmail.com>
2025-09-06 15:45:02 +0200

    Remove EmoticonEditorView

--- MyCalendar/MyCalendar/DayEntry.swift
+++ MyCalendar/MyCalendar/DayEntry.swift
@@ -7,23 +7,68 @@
 
 import Foundation
 import SwiftData
-import UIKit // Needed for UIImage data
+import UIKit
 
+// MARK: - TextStickerInfo Model
+@Model
+final class TextStickerInfo {
+    @Attribute(.unique) var id = UUID()
+    var text: String
+    var posX: CGFloat = 0.0
+    var posY: CGFloat = 0.0
+    var scale: CGFloat = 1.0
+    
+    @Relationship(inverse: \DayEntry.textStickers)
+    var dayEntry: DayEntry?
+    
+    init(text: String, posX: CGFloat = 0.0, posY: CGFloat = 0.0, scale: CGFloat = 1.0) {
+        self.text = text
+        self.posX = posX
+        self.posY = posY
+        self.scale = scale
+    }
+}
+
+// MARK: - EmoticonInfo Model
+@Model
+final class EmoticonInfo {
+    @Attribute(.unique) var id = UUID()
+    var character: String
+    var posX: CGFloat = 0.0
+    var posY: CGFloat = 0.0
+    var scale: CGFloat = 1.0
+    
+    @Relationship(inverse: \DayEntry.emoticons)
+    var dayEntry: DayEntry?
+    
+    init(character: String, posX: CGFloat = 0.0, posY: CGFloat = 0.0, scale: CGFloat = 1.0) {
+        self.character = character
+        self.posX = posX
+        self.posY = posY
+        self.scale = scale
+    }
+}
+
+// MARK: - DayEntry Model
 @Model
 final class DayEntry {
-    @Attribute(.unique) var date: Date // Use a specific date as a unique ID
-    var backgroundImageData: Data?     // To store the background photo
-    var drawingData: Data?             // To store the PKDrawing
+    @Attribute(.unique) var date: Date
+    var backgroundImageData: Data?
+    var drawingData: Data?
     
-    var backgroundImageScale: CGFloat = 1.0 // 1.0 means no zoom
-    var backgroundImageOffsetX: CGFloat = 0.0 // 0.0 means centered
-    var backgroundImageOffsetY: CGFloat = 0.0 // 0.0 means centered
+    var backgroundImageScale: CGFloat = 1.0
+    var backgroundImageOffsetX: CGFloat = 0.0
+    var backgroundImageOffsetY: CGFloat = 0.0
     
-    // This sets up a one-to-many relationship with EmoticonInfo
+    // Stickers
     @Relationship(deleteRule: .cascade, inverse: \EmoticonInfo.dayEntry)
     var emoticons: [EmoticonInfo] = []
+    
+    @Relationship(deleteRule: .cascade, inverse: \TextStickerInfo.dayEntry)
+    var textStickers: [TextStickerInfo] = []
 
     init(date: Date) {
         self.date = date
     }
 }
+
