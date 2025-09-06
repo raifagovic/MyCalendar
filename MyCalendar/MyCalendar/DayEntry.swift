6a7201933c2142ccee5ac0b69f95c900b7af8274
Raif Agovic <raif.agovic.dev@gmail.com>
2025-09-06 22:26:56 +0200

    Refactor DayEntry to use unified StickerInfo model

--- MyCalendar/MyCalendar/DayEntry.swift
+++ MyCalendar/MyCalendar/DayEntry.swift
@@ -9,66 +9,27 @@ import Foundation
 import SwiftData
 import UIKit
 
-// MARK: - TextStickerInfo Model
-@Model
-final class TextStickerInfo {
-    @Attribute(.unique) var id = UUID()
-    var text: String
-    var posX: CGFloat = 0.0
-    var posY: CGFloat = 0.0
-    var scale: CGFloat = 1.0
-    
-    @Relationship(inverse: \DayEntry.textStickers)
-    var dayEntry: DayEntry?
-    
-    init(text: String, posX: CGFloat = 0.0, posY: CGFloat = 0.0, scale: CGFloat = 1.0) {
-        self.text = text
-        self.posX = posX
-        self.posY = posY
-        self.scale = scale
-    }
-}
-
-// MARK: - EmoticonInfo Model
-@Model
-final class EmoticonInfo {
-    @Attribute(.unique) var id = UUID()
-    var character: String
-    var posX: CGFloat = 0.0
-    var posY: CGFloat = 0.0
-    var scale: CGFloat = 1.0
-    
-    @Relationship(inverse: \DayEntry.emoticons)
-    var dayEntry: DayEntry?
-    
-    init(character: String, posX: CGFloat = 0.0, posY: CGFloat = 0.0, scale: CGFloat = 1.0) {
-        self.character = character
-        self.posX = posX
-        self.posY = posY
-        self.scale = scale
-    }
-}
-
-// MARK: - DayEntry Model
 @Model
 final class DayEntry {
+    // Unique identifier: the date itself
     @Attribute(.unique) var date: Date
-    var backgroundImageData: Data?
-    var drawingData: Data?
     
+    // Background image
+    var backgroundImageData: Data?
+    var drawingData: Data? // optional, for PencilKit later
+    
+    // Transform state for background
     var backgroundImageScale: CGFloat = 1.0
     var backgroundImageOffsetX: CGFloat = 0.0
     var backgroundImageOffsetY: CGFloat = 0.0
     
-    // Stickers
-    @Relationship(deleteRule: .cascade, inverse: \EmoticonInfo.dayEntry)
-    var emoticons: [EmoticonInfo] = []
+    // Stickers (both text + emoji, handled by StickerInfo)
+    @Relationship(deleteRule: .cascade, inverse: \StickerInfo.dayEntry)
+    var stickers: [StickerInfo] = []
     
-    @Relationship(deleteRule: .cascade, inverse: \TextStickerInfo.dayEntry)
-    var textStickers: [TextStickerInfo] = []
-
     init(date: Date) {
         self.date = date
     }
 }
+
 
