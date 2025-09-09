af600adf653830f218d7b35687cbd5ba392e8c98
Raif Agovic <raif.agovic.dev@gmail.com>
2025-09-09 11:50:40 +0200

    Store positions relative (0…1) instead of absolute pixels in StickerInfo

--- MyCalendar/MyCalendar/StickerInfo.swift
+++ MyCalendar/MyCalendar/StickerInfo.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftData
+import CoreGraphics
 
 @Model
 final class StickerInfo {
@@ -14,31 +15,29 @@ final class StickerInfo {
         case text
         case emoji
     }
-    
+
     var type: StickerType
-    var content: String          // Either text or emoji characters
-    
-    // Positioning and transform
-    var posX: CGFloat = 0.0
-    var posY: CGFloat = 0.0
+    var content: String
+
+    // Relative positioning (0.0…1.0)
+    var relativePosX: CGFloat = 0.5
+    var relativePosY: CGFloat = 0.5
     var scale: CGFloat = 1.0
-    
-    // (Optional) Rotation if you add it later
-    // var rotation: Double = 0.0
-    
+
     // Relationship back to DayEntry
     var dayEntry: DayEntry?
-    
+
     init(type: StickerType,
          content: String,
-         posX: CGFloat = 0.0,
-         posY: CGFloat = 0.0,
+         relativePosX: CGFloat = 0.5,
+         relativePosY: CGFloat = 0.5,
          scale: CGFloat = 1.0) {
         self.type = type
         self.content = content
-        self.posX = posX
-        self.posY = posY
+        self.relativePosX = relativePosX
+        self.relativePosY = relativePosY
         self.scale = scale
     }
 }
+
 
