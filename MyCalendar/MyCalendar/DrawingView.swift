d05d142e01a68b24c806c4cdb15c590ee672be9b
Raif Agovic <raif.agovic.dev@gmail.com>
2025-09-25 10:12:24 +0200

    Update DrawingView

--- MyCalendar/MyCalendar/DrawingView.swift
+++ MyCalendar/MyCalendar/DrawingView.swift
@@ -9,13 +9,52 @@ import SwiftUI
 import PencilKit
 
 struct DrawingView: UIViewRepresentable {
-    @Binding var canvasView: PKCanvasView
+    @Binding var drawingData: Data?
+    var isEditable: Bool
 
-    func makeUIView(context: Context) -> PKCanvasView {
-        canvasView.drawingPolicy = .anyInput
-        canvasView.tool = PKInkingTool(.pen, color: .black, width: 5)
-        return canvasView
+    class Coordinator: NSObject, PKCanvasViewDelegate {
+        var parent: DrawingView
+
+        init(parent: DrawingView) {
+            self.parent = parent
+        }
+
+        func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+            parent.drawingData = canvasView.drawing.dataRepresentation()
+        }
     }
 
-    func updateUIView(_ uiView: PKCanvasView, context: Context) {}
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+
+    func makeUIView(context: Context) -> PKCanvasView {
+        let canvas = PKCanvasView()
+        canvas.delegate = context.coordinator
+        canvas.backgroundColor = .clear
+        canvas.isOpaque = false
+        canvas.drawingPolicy = .anyInput
+        canvas.isUserInteractionEnabled = isEditable
+
+        // Restore saved drawing if available
+        if let data = drawingData,
+           let drawing = try? PKDrawing(data: data) {
+            canvas.drawing = drawing
+        }
+
+        return canvas
+    }
+
+    func updateUIView(_ uiView: PKCanvasView, context: Context) {
+        uiView.isUserInteractionEnabled = isEditable
+
+        if let data = drawingData,
+           let drawing = try? PKDrawing(data: data) {
+            // Prevent overwriting unsaved work
+            if drawing != uiView.drawing {
+                uiView.drawing = drawing
+            }
+        }
+    }
 }
+
