765923e73781d0216413d42a1e1425464856e2aa
Raif Agovic <raif.agovic.dev@gmail.com>
2025-10-09 11:12:14 +0200

    Modify Date+Extension.swift to allow for a more specific ID

--- MyCalendar/MyCalendar/Date+Extensions.swift
+++ MyCalendar/MyCalendar/Date+Extensions.swift
@@ -6,18 +6,37 @@
 //  Created by Raif Agovic on 5. 8. 2025..
 //
 
+//import Foundation
+//
+//// This extension adds useful computed properties to the built-in Date type.
+//extension Date: Identifiable {
+//    
+//    var startOfMonth: Date {
+//        let calendar = Calendar.current
+//        let components = calendar.dateComponents([.year, .month], from: self)
+//        return calendar.date(from: components) ?? self
+//    }
+//    
+//    // While we're here, let's also restore the Identifiable conformance
+//    // in this centralized location.
+//    public var id: Date { self }
+//}
+
 import Foundation
 
-// This extension adds useful computed properties to the built-in Date type.
 extension Date: Identifiable {
-    
+    public var id: Date { self } // This remains the default
+
     var startOfMonth: Date {
         let calendar = Calendar.current
         let components = calendar.dateComponents([.year, .month], from: self)
         return calendar.date(from: components) ?? self
     }
-    
-    // While we're here, let's also restore the Identifiable conformance
-    // in this centralized location.
-    public var id: Date { self }
+
+    // NEW: A computed property to create a unique ID for the *grid start* of a month
+    var gridStartID: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd-grid-start"
+        return formatter.string(from: self)
+    }
 }
