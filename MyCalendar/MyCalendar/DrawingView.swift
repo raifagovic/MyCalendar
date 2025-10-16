8f38f00ba0d61355e81e4302a622144185837463
Raif Agovic <raif.agovic.dev@gmail.com>
2025-10-16 20:55:03 +0200

    Replace deprecated PKToolPicker.shared(for:) with new PKToolPicker instance

--- MyCalendar/MyCalendar/DrawingView.swift
+++ MyCalendar/MyCalendar/DrawingView.swift
@@ -47,8 +47,8 @@ struct DrawingView: UIViewRepresentable {
         if showToolPicker {
             DispatchQueue.main.async {
                 if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                   let window = windowScene.windows.first,
-                   let toolPicker = PKToolPicker.shared(for: window) {
+                   let window = windowScene.windows.first {
+                   let toolPicker = PKToolPicker()
                     toolPicker.setVisible(true, forFirstResponder: canvas)
                     toolPicker.addObserver(canvas)
                     canvas.becomeFirstResponder()
