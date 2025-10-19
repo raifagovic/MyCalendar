280ac32dbb2c11e6b91ac05cb5f0c35f2191eb7d
Raif Agovic <raif.agovic.dev@gmail.com>
2025-10-19 16:24:36 +0200

    Remove redundant comment from MyCalendarApp

--- MyCalendar/MyCalendar/MyCalendarApp.swift
+++ MyCalendar/MyCalendar/MyCalendarApp.swift
@@ -5,33 +5,6 @@
 //  Created by Raif Agovic on 3. 7. 2025..
 //
 
-//import SwiftUI
-//import SwiftData
-//
-//@main
-//struct MyCalendarApp: App {
-//    var sharedModelContainer: ModelContainer = {
-//        let schema = Schema([
-//            DayEntry.self,
-//            StickerInfo.self // ✅ Use StickerInfo instead of EmoticonInfo
-//        ])
-//        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-//
-//        do {
-//            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-//        } catch {
-//            fatalError("Could not create ModelContainer: \(error)")
-//        }
-//    }()
-//
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//        }
-//        .modelContainer(sharedModelContainer)
-//    }
-//}
-
 import SwiftUI
 import SwiftData
 
