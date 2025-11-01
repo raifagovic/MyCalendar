4725f5c28000fc586fe29c35362b91986f754d0a
Raif Agovic <raif.agovic.dev@gmail.com>
2025-11-01 11:01:10 +0100

    Simplify DrawingView and improve drawing data binding

--- MyCalendar/MyCalendar/DrawingView.swift
+++ MyCalendar/MyCalendar/DrawingView.swift
@@ -5,6 +5,58 @@
 //  Created by Raif Agovic on 7. 7. 2025..
 //
 
+//import SwiftUI
+//import PencilKit
+//
+//struct DrawingView: UIViewRepresentable {
+//    @ObservedObject var controller: DrawingController
+//    @Binding var drawingData: Data?
+//
+//    func makeUIView(context: Context) -> UIView {
+//        let host = CanvasHostView(frame: .zero)
+//        host.controller = controller
+//
+//        let canvas = host.canvasView
+//        canvas.delegate = context.coordinator
+//
+//        controller.canvas = canvas
+//
+//        // If drawing mode is already active, try showing tool picker
+//        if controller._isDrawing {
+//            DispatchQueue.main.async {
+//                controller.ensureShowToolPickerWithRetry()
+//            }
+//        }
+//
+//        return host
+//    }
+//
+//    func updateUIView(_ uiView: UIView, context: Context) {
+//        guard let host = uiView as? CanvasHostView else { return }
+//        let canvas = host.canvasView
+//        controller.canvas = canvas
+//        canvas.delegate = context.coordinator
+//
+//        if let data = drawingData,
+//           let drawing = try? PKDrawing(data: data),
+//           drawing != canvas.drawing {
+//            canvas.drawing = drawing
+//        }
+//    }
+//    
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator(parent: self)
+//    }
+//
+//    class Coordinator: NSObject, PKCanvasViewDelegate {
+//        var parent: DrawingView
+//        init(parent: DrawingView) { self.parent = parent }
+//        func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+//            parent.drawingData = canvasView.drawing.dataRepresentation()
+//        }
+//    }
+//}
+
 import SwiftUI
 import PencilKit
 
@@ -12,45 +64,36 @@ struct DrawingView: UIViewRepresentable {
     @ObservedObject var controller: DrawingController
     @Binding var drawingData: Data?
 
-    func makeUIView(context: Context) -> UIView {
-        let host = CanvasHostView(frame: .zero)
+    func makeUIView(context: Context) -> CanvasHostView {
+        let host = CanvasHostView()
         host.controller = controller
 
-        let canvas = host.canvasView
-        canvas.delegate = context.coordinator
-
-        controller.canvas = canvas
-
-        // If drawing mode is already active, try showing tool picker
-        if controller._isDrawing {
-            DispatchQueue.main.async {
-                controller.ensureShowToolPickerWithRetry()
-            }
-        }
+        host.canvasView.delegate = context.coordinator
+        controller.canvas = host.canvasView
 
         return host
     }
 
-    func updateUIView(_ uiView: UIView, context: Context) {
-        guard let host = uiView as? CanvasHostView else { return }
-        let canvas = host.canvasView
-        controller.canvas = canvas
-        canvas.delegate = context.coordinator
+    func updateUIView(_ host: CanvasHostView, context: Context) {
+        controller.canvas = host.canvasView
 
         if let data = drawingData,
            let drawing = try? PKDrawing(data: data),
-           drawing != canvas.drawing {
-            canvas.drawing = drawing
+           drawing != host.canvasView.drawing {
+            host.canvasView.drawing = drawing
         }
+
+        host.canvasView.delegate = context.coordinator
     }
-    
+
     func makeCoordinator() -> Coordinator {
-        Coordinator(parent: self)
+        Coordinator(self)
     }
 
     class Coordinator: NSObject, PKCanvasViewDelegate {
         var parent: DrawingView
-        init(parent: DrawingView) { self.parent = parent }
+        init(_ parent: DrawingView) { self.parent = parent }
+
         func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
             parent.drawingData = canvasView.drawing.dataRepresentation()
         }
